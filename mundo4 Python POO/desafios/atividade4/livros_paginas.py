064ecf575fc13b4303a56c8550e0b5108a843ef2
Miguel957-dev <miguellucaslopesdequeirozmigue@gmail.com>
2026-07-17 19:18:36 -0300

    Atividade 4 completa

--- mundo4 Python POO/desafios/atividade4/livros_paginas.py
+++ mundo4 Python POO/desafios/atividade4/livros_paginas.py
@@ -12,17 +12,26 @@ class Livro:
 
     def avançar_paginas(self, paginas):
         destino = self.contador + paginas
+        if self.contador == 1:
+            print(f"[blue]Você acabou de abrir o livro '[red]{self.titulo}[/red]' que tem [green]{self.paginas_do_livro}[/green] páginas no total. Você agora está na [yellow]página 1.[/yellow] [/blue]")
         
         if destino <= self.paginas_do_livro:
             for p in range(self.contador + 1 , destino + 1):
-
-                print(p)
+                if p == destino:
+                    print(f'Pág{p} ▶ [blue]Você avançou {paginas} páginas e agora está na [/blue][yellow] página {destino}[/yellow]')
+                    sleep(0.5)
+                else:print(f'Pág{p} ▶ ', end= '') 
+                sleep(0.5) 
             self.contador += paginas
         
         if destino > self.paginas_do_livro:
             self.paginas_do_livro += 1
             for p in range(self.contador + 1 , self.paginas_do_livro):
-                print(p)
+                if self.contador == self.paginas_do_livro:
+                    print(f'Pág{p} ▶ [blue]Você avançou {paginas} páginas e agora está na [/blue][yellow] página {destino}[/yellow]', end= '')
+                    sleep(0.5)
+                else: print(f'Pág{p} ▶ ', end= '')
+                sleep(0.5)
             self.contador += paginas
             print('[red]Você ultrapassou o número de paginas exitente no livro. [/red]')
 
@@ -31,4 +40,5 @@ class Livro:
 
 l1 = Livro('Principe',  20)
 l1.avançar_paginas(5)
-l1.avançar_paginas(20)
+l1.avançar_paginas(7)
+l1.avançar_paginas(10)

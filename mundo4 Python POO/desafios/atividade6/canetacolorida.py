b75b6926c46eaf08a49fb59af86751e3575a99ac
Miguel957-dev <miguellucaslopesdequeirozmigue@gmail.com>
2026-07-17 22:33:04 -0300

    Atividade 6

---
+++ mundo4 Python POO/desafios/atividade6/canetacolorida.py
@@ -0,0 +1,42 @@
+from rich import print
+
+class Caneta:
+    def __init__(self, cor = 'azul'):
+        escolha = ""
+        match cor.lower().strip():
+            case "azul":
+                escolha = "[blue]"
+            case "vermelho" | "vermelha":
+                escolha = "[red]"
+            case "verde":
+                escolha = "[green]"
+            case _:
+                escolha = "[white]"
+        self.cor = escolha
+        self.tampada = True
+    
+    def escrever(self, msg):
+        if self.tampada:
+            print(f':prohibited: A {self.cor} caneta[/] esta tampada')
+        else:
+            print(f"{self.cor} {msg} [/]")
+        
+    def quebrar_linha(self, qtd = 1):
+        print("\n" * qtd, end='')
+        pass
+
+    def tampar(self):
+        self.tampada = True
+
+    def destampar(self):
+        self.tampada = False
+
+c1 = Caneta('Azul')
+c2 = Caneta('vermelho')
+c3 = Caneta('verde')
+
+c1.destampar()
+c1.quebrar_linha(2)
+c1.escrever("Olá mundo")
+c2.escrever("Funciona")
+c3.escrever("Deu certo")

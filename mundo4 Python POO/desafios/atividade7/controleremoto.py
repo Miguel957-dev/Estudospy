fe5ebcd2c77b63567b8dfb7c6f38f565b1f4c630
Miguel957-dev <miguellucaslopesdequeirozmigue@gmail.com>
2026-08-17 10:04:32 -0300

    Parte da atividade

--- mundo4 Python POO/desafios/atividade7/controleremoto.py
+++ mundo4 Python POO/desafios/atividade7/controleremoto.py
@@ -7,12 +7,14 @@ class ControleRemoto:
     volume_min:int = 1
     volume_max:int = 5
 
-    def __init__(self,  canal = 1, volume = 2):
+
+    def __init__(self,  canal = 3, volume = 2):
         self.canal_atual:int = canal
         self.volume_atual:int = volume
         self.ligado:bool = False
 
-    
+    def ligar_desligar(self):
+        self.ligado = not self.ligado
 
     def mostrar_tv(self):
         
@@ -20,10 +22,18 @@ class ControleRemoto:
         if not self.ligado:
             conteudo = f"[red]A TV está desligada[/red]"
         else:
-            conteudo = f"[green]CANAL e VOLUME[/green]"
+            conteudo = f"[green]CANAL = [/green]"
+            for canal in range(ControleRemoto.canal_min, ControleRemoto.canal_max + 1):
+                if canal == self.canal_atual:
+                    conteudo += f" [black on yellow] {canal} [/] "
+                else:
+                    conteudo += f" {canal} "
+
+
 
         tv = Panel(conteudo, title = "[TV]")
         print(tv)
 
 c = ControleRemoto()
+c.ligar_desligar()
 c.mostrar_tv()

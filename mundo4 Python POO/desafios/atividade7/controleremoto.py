b7111c64c93877dd0233f47114d448aa0ccd56e7
Miguel957-dev <miguellucaslopesdequeirozmigue@gmail.com>
2026-08-17 18:15:05 -0300

    Atividade quase completa

--- mundo4 Python POO/desafios/atividade7/controleremoto.py
+++ mundo4 Python POO/desafios/atividade7/controleremoto.py
@@ -5,13 +5,39 @@ class ControleRemoto:
     canal_min:int = 1
     canal_max:int = 6
     volume_min:int = 1
-    volume_max:int = 5
+    volume_max:int = 10
 
 
-    def __init__(self,  canal = 3, volume = 2):
+    def __init__(self,  canal = 1, volume = 1):
         self.canal_atual:int = canal
         self.volume_atual:int = volume
         self.ligado:bool = False
+
+    def canal_mais(self):
+        if self.ligado:
+            if self.canal_atual == ControleRemoto.canal_max:
+                self.canal_atual = ControleRemoto.canal_min
+            else:
+                self.canal_atual += 1
+
+    def canal_menos(self):
+        if self.ligado:
+            if self.canal_atual > ControleRemoto.canal_min:
+                self.canal_atual -= 1
+
+    def volume_mais(self):
+        if self.ligado:
+            if self.volume_atual < ControleRemoto.volume_max:
+                self.volume_atual += 1
+                
+    def volume_menos(self):
+        if self.ligado:
+            if self.volume_atual == ControleRemoto.volume_min:
+                self.volume_atual = self.volume_atual
+
+            else:
+                self.volume_atual -= 1
+
 
     def ligar_desligar(self):
         self.ligado = not self.ligado
@@ -25,15 +51,21 @@ class ControleRemoto:
             conteudo = f"[green]CANAL = [/green]"
             for canal in range(ControleRemoto.canal_min, ControleRemoto.canal_max + 1):
                 if canal == self.canal_atual:
-                    conteudo += f" [black on yellow] {canal} [/] "
+                    conteudo += f" [yellow on yellow] {canal} [/] "
                 else:
                     conteudo += f" {canal} "
 
+            conteudo += f"\nVOLUME = "
+            for volume in range(ControleRemoto.volume_min, ControleRemoto.volume_max + 1):
+                if volume <= self.volume_atual:
+                    conteudo += f"[black on cyan]  [/]"
+                else:
+                    conteudo += f"[black on white]  [/]"
 
 
-        tv = Panel(conteudo, title = "[TV]")
+        tv = Panel(conteudo, title = "[TV]", width = 30)
         print(tv)
 
-c = ControleRemoto()
+c = ControleRemoto(3, 7)
 c.ligar_desligar()
 c.mostrar_tv()

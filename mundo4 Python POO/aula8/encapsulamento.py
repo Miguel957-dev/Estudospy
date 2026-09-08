237c778398c2c6a85541fc4c6cbd52b78f3ce362
Miguel957-dev <miguellucaslopesdequeirozmigue@gmail.com>
2026-09-08 15:41:37 -0300

    Teoria de Encapsulamento

---
+++ mundo4 Python POO/aula8/encapsulamento.py
@@ -0,0 +1,11 @@
+""" ENCAPSULAMENTO """
+
+# Encapsulamento visa manter a integridade do sistema, protegendo o estado interno do objeto contra interferencia externa não autorizada, tipo eu sem autorização deixar apertar o botão de uma bomba 
+
+#VANTAGENS: Seguraça e controle, mais facil da manuntenção, Flexibilidade e reutilização, Redução de efeitos colaterais
+""" TOPICOS
+VISIBILIDADE, Existem 3 tipos de visibilidade
+public + : Visto por todos, ate mesmo fora das classes, feito o atributo normal
+protected # :so pode ser acessado por fihas da clssa mas nunca pelo codigo principal _ antes do atributo
+private - : so pode ser visto no codigo principal __ antes do atributos"""
+#Consenting Adults 
---
+++ mundo4 Python POO/aula8/main.py
@@ -0,0 +1,10 @@
+from ex008 import ContaBancaria 
+
+def main():
+    c1 = ContaBancaria(111, "Miguel", 6000)
+    c1.depositar(500)
+
+    print(c1)
+
+if __name__=="__main__":
+    main() 

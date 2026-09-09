b04bb619dde196084743108c6ee81ae8dfc6adba
Miguel957-dev <miguellucaslopesdequeirozmigue@gmail.com>
2026-09-09 17:16:57 -0300

    .

---
+++ FastAPI/aula2/main.py
@@ -0,0 +1,17 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+
+# para rodar o codigo executar no terminal, uvicorn main:app --reload
+
+#endpoint:
+#ordens
+
+#CRUD
+
+#Rest APIs
+#Get -> Leitura de informação /pegar
+#Pots -> enviar/criar
+#Put/Patch -> edição
+#Delete -> deletar

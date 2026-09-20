fa89742f31b3380700c4147bdc844f283ba7f5db
Miguel957-dev <miguellucaslopesdequeirozmigue@gmail.com>
2026-09-20 13:35:23 -0300

    Classe retangulo

---
+++ mundo4 Python POO/desafios/atividade16/main.py
@@ -0,0 +1,9 @@
+from retangulo import *
+
+def main():
+    d = Retangulo()
+    d.base = 8
+    d.altura = 8
+    d.medidas
+if __name__ == "__main__":
+    main()
---
+++ mundo4 Python POO/desafios/atividade16/retangulo.py
@@ -0,0 +1,37 @@
+class Retangulo():
+    def __init__(self, base = 1, altura = 1):
+        self.base = base
+        self.altura = altura 
+
+    @property
+    def medidas(self):
+        print(f"Base = {self._base} \n",
+        f"Altura = {self._altura} \n",
+        f"Area = {self._base * self._altura}")
+
+    @property
+    def base(self):
+        return self._base
+
+    @base.setter
+    def base(self, base):
+        if base <= 0:
+            raise ValueError('Valor da base inválido')
+        else:
+            self._base = base
+
+    @property
+    def altura(self):
+        return self._altura
+
+    @altura.setter
+    def altura(self, altura):
+        if altura <= 0:
+            raise ValueError("Valor da altura invalído")
+        else:
+            self._altura = altura
+
+    @property
+    def calculo_area(self):
+        self.area = self._altura * self._base
+        return self.area
